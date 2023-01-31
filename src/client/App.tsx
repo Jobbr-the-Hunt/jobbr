dc3e0576b7a71a14e389c7f69f785bbaea2ba967
Dennis Cheung <dennis@Denniss-MacBook-Air.local>
2023-01-31 08:22:30 -0800

    adds basic routes, componets, and containers to the application

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { render } from 'react-dom';
+
+import Navbar from './container/Navbar';
+import MainContent from './container/MainContent';
+import Login from './container/Login';
 
 const App = () => {
   return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<MainContent />} />
+          <Route path="Login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/client/container/JobTiles.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function JobTiles() {
+  return (
+    <div>
+      <h1>Job Tiles</h1>
+    </div>
+  );
+}
+
+export default JobTiles;
